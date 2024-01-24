65ee134b1c0ae0c639be9f0634f7f4aa7eafa40f
pollosatyi <129182414+DrShadowich@users.noreply.github.com>
2024-01-24 20:31:12 +0300

    Добавьте файлы проекта.

--- Program.cs
+++ Program.cs
@@ -1,36 +1,77 @@
-﻿using Microsoft.Data.Sqlite;
+﻿//using Microsoft.Data.Sqlite;
+//using System.Data;
+
+//var connection = new SqliteConnection("Data Source = MainDate.db");// вводим имя переменной
+//connection.Open();//открываем соединение
+
+////подготовим команду которую отправляем в б/д
+//var comand = new SqliteCommand(); // Создали переменную для команды 
+////устанавливаем связь б/д по данному соединению  для команды
+//comand.Connection = connection;
+//comand.CommandType = CommandType.Text;
+//comand.CommandText = @"CREATE TABLE [MainTable] (
+//	[id]	INTEGER NOT NULL UNIQUE,
+//	[UserId] INTEGER NOT NULL,
+//	[time]	datetime,
+//	[Price]	float,
+//	[Count]	INTEGER,
+//	[TypeOrderId]	INTEGER,
+//	PRIMARY KEY([id] AUTOINCREMENT)
+//);
+
+//CREATE TABLE [USER] (
+//	[id]	INTEGER NOT NULL UNIQUE,
+//	[FullName]	TEXT NOT NULL,
+//	[ShortName]	TEXT NOT NULL,
+//	PRIMARY KEY([id] AUTOINCREMENT)
+//);
+
+//CREATE TABLE [OrderTypes] (
+//	[id] INTEGER NOT NULL UNIQUE,
+//	[TypeOrderNow]	INTEGER,
+//	[OrderLimit]	INTEGER,
+//	PRIMARY KEY([id] AUTOINCREMENT)
+//);";
+//comand.ExecuteNonQuery();//отправляет запрос в б/д
+//connection.Close();//закрываем соединение в конце работы
+
+
+using Microsoft.Data.Sqlite;
 using System.Data;
 
-var connection = new SqliteConnection("Data Source = MainDate.db");// вводим имя переменной
+var connectionStringBuilder = new SqliteConnectionStringBuilder(); // создаем экземпляр ConnectionStringBuilder
+connectionStringBuilder.DataSource = "MainDate.db"; // задаем имя источника данных
+
+var connection = new SqliteConnection(connectionStringBuilder.ConnectionString);// вводим имя переменной
 connection.Open();//открываем соединение
 
 //подготовим команду которую отправляем в б/д
-var comand = new SqliteCommand(); // Создали переменную для команды 
+var comand = new SqliteCommand(); // Создаем переменную для команды 
 //устанавливаем связь б/д по данному соединению  для команды
 comand.Connection = connection;
 comand.CommandType = CommandType.Text;
 comand.CommandText = @"CREATE TABLE [MainTable] (
 	[id]	INTEGER NOT NULL UNIQUE,
-	[UserId] INTEGER NOT NULL,
+ [UserId] INTEGER NOT NULL,
 	[time]	datetime,
 	[Price]	float,
 	[Count]	INTEGER,
 	[TypeOrderId]	INTEGER,
-	PRIMARY KEY([id] AUTOINCREMENT)
+ PRIMARY KEY([id] AUTOINCREMENT)
 );
 
 CREATE TABLE [USER] (
 	[id]	INTEGER NOT NULL UNIQUE,
 	[FullName]	TEXT NOT NULL,
 	[ShortName]	TEXT NOT NULL,
-	PRIMARY KEY([id] AUTOINCREMENT)
+ PRIMARY KEY([id] AUTOINCREMENT)
 );
 
 CREATE TABLE [OrderTypes] (
-	[id] INTEGER NOT NULL UNIQUE,
+ [id] INTEGER NOT NULL UNIQUE,
 	[TypeOrderNow]	INTEGER,
 	[OrderLimit]	INTEGER,
-	PRIMARY KEY([id] AUTOINCREMENT)
+ PRIMARY KEY([id] AUTOINCREMENT)
 );";
 comand.ExecuteNonQuery();//отправляет запрос в б/д
 connection.Close();//закрываем соединение в конце работы
